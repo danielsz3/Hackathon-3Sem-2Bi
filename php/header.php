c384584ce472792d1540524870a7475a98635a23
João Gabryel <126635450+JoaoGabryel@users.noreply.github.com>
2024-06-18 22:00:49 -0300

    Atualização do código de gerenciamento de url

--- php/header.php
+++ php/header.php
@@ -61,3 +61,19 @@
         </div>
 
     </header>
+    <?php
+    if (isset($_GET['param'])) { 
+        $page = explode("/", $_GET['param']); 
+        $pasta = $page[0] ?? NULL; 
+        $arquivo = $page[1] ?? NULL; 
+        $id = $page[2] ?? NULL; 
+        $page = "$pasta/$arquivo";
+        require "header.php"; 
+        if (file_exists("$page.php")) {
+            require "$page.php";
+        } else {
+            require "paginas/erro.php"; 
+        }
+        require "footer.php"; 
+    }
+    ?>
