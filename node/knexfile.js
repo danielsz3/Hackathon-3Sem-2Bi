fcd9b89a46bff17ca82ed0874b623cd9674bf78b
Daniel Oliveira <danielmo352@gmail.com>
2024-06-19 23:59:09 -0300

    knex

--- node/knexfile.js
+++ node/knexfile.js
@@ -1,4 +1,4 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
     development: {
@@ -24,4 +24,4 @@ module.exports = {
         },
         useNullAsDefault: true
     }
-};
+}
