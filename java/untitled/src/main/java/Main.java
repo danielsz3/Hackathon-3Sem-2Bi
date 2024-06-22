afaaa45f0afd03f73a896484b5b0c3d676e0d75a
Daniel Oliveira <danielmo352@gmail.com>
2024-06-22 17:54:44 -0300

    Update Main.java

--- java/untitled/src/main/java/Main.java
+++ java/untitled/src/main/java/Main.java
@@ -6,7 +6,7 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            new MenuHub();
+            SwingUtilities.invokeLater(MenuHub::new);
         });
     }
 }
