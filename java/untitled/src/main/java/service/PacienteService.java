9ceb44f22290129bf31a917f20189dfc6fb9b2f7
devfelipegustavo <143041056+devfelipegustavo@users.noreply.github.com>
2024-06-21 21:22:02 -0300

    Ajustando Metódos
    Metodo getIdEnderec

--- java/untitled/src/main/java/service/PacienteService.java
+++ java/untitled/src/main/java/service/PacienteService.java
@@ -7,17 +7,23 @@ import model.Paciente;
 
 public class PacienteService {
     public void salvar(Endereco endereco, Paciente paciente) {
+    public void salvar(Paciente paciente) {
         try {
             var daoEndereco = new EnderecoDao();
             var daoPaciente = new PacienteDao();
+            System.out.println(paciente.getEndereco().getId());
+            System.out.println(paciente);
+
             if(paciente.getId() == null){
                 var consulta = daoEndereco.consultarIdPorCep(endereco.getCep());
                 if (consulta > 0){
                     endereco.setId(consulta);
+
                 } else {
                     daoEndereco.inserir(endereco);
                     endereco.setId(daoEndereco.consultarMaiorId());
                 }
+                System.out.println(daoEndereco.consultarMaiorId());
                 daoPaciente.inserir(paciente,endereco);
             }
         }catch(Exception e){
