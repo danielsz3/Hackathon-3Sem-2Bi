45010b712f582f8a70f69cdcdecd07350d87f442
Gabriel Capoia <gabrielcapoia11@gmail.com>
2024-06-22 12:27:54 -0300

    Post paciente, falta conectar com o endereco

--- node/knexfile.js
+++ node/knexfile.js
@@ -4,10 +4,10 @@ module.exports = {
     development: {
         client: 'mysql',
         connection: {
-            host: '127.0.0.1',
-            port: 3333,
+            host: 'localhost',
+            port: 3306,
             user: 'root',
-            password: 'daniel',
+            password: '',
             database: 'dbvacinacao',
             insecureAuth: true,
         },
